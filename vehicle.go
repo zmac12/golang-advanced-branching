e2319622e9a17fe1c08bb25f9472d1c36f24929a
Ashish Minocha <53531651+ashmintech@users.noreply.github.com>
2020-03-09 15:13:20 -0300

    Update vehicle.go

--- vehicle.go
+++ vehicle.go
@@ -4,47 +4,10 @@ import (
 
 )
 
-type vehicle interface {
-}
-
-type car struct {
-	model       string
-	make        string
-	typeVehicle string
-}
-
-type truck struct {
-	model       string
-	make        string
-	typeVehicle string
-}
-
-type bike struct {
-	model string
-	make  string
-}
 
 // Values array for the feedback.json file
-type Values struct {
-	Models []Model `json:"values"`
-}
 
 // Model array for the feedback.json file
-type Model struct {
-	Name     string   `json:"model"`
-	Feedback []string `json:"feedback"`
-}
-
-type feedbackResult struct {
-	feedbackTotal    int
-	feedbackPositive int
-	feedbackNegative int
-	feedbackNeutral  int
-}
-
-var vehicleResult map[string]feedbackResult
-
-var inventory []vehicle
 
 type rating float32
 
@@ -58,6 +21,7 @@ const (
 
 func init() {
 
+/*
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -70,8 +34,8 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-
-	vehicleResult = make(map[string]feedbackResult)
+*/
+//	vehicleResult = make(map[string]feedbackResult)
 
 }
 
