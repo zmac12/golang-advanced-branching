e41a615863536f741ed39c235c53be2578268d1e
Ashish Minocha <53531651+ashmintech@users.noreply.github.com>
2020-04-17 22:40:08 -0300

    Add files via upload

--- vehicle.go
+++ vehicle.go
@@ -4,10 +4,51 @@ import (
 
 )
 
+type vehicle interface {
+
+}
+
+type car struct {
+	model string
+	make string
+	typeVehicle string
+}
+
+type truck struct {
+	model string
+	make string
+	typeVehicle string
+}
+
+type bike struct {
+	model string
+	make string
+}
+
 
 // Values array for the feedback.json file
+type Values struct {
+	Models []Model `json:"values"`
+}
 
 // Model array for the feedback.json file
+type Model struct {
+	Name string `json:"model"`
+	Feedback []string `json:"feedback"`
+}
+
+type feedbackResult struct {
+	feedbackTotal	int
+	feedbackPositive int
+	feedbackNegative int
+	feedbackNeutral int
+}
+
+var vehicleResult map[string]feedbackResult
+
+var inventory []vehicle
+
+
 
 type rating float32
 
@@ -21,7 +62,7 @@ const (
 
 func init() {
 
-/*
+
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -34,20 +75,22 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
 func main() {
 
 	// Generate ratings for the different vehicles
-	
+
 	// Print ratings for the different vehicles
+
 }
 
 /*
 func readJSONFile() Values {
+
 	jsonFile, err := os.Open("feedback.json")
 
 	if err != nil {
