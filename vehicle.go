45b8c8706540925d50d2f2f59a2f1f61f6e5b9cf
Zach <zach.mcquiston35@gmail.com>
2022-09-21 16:16:04 -0600

    Adding module 2 code

--- vehicle.go
+++ vehicle.go
@@ -109,30 +109,30 @@ func generateRating() {
 		var vehRating rating
 		for _, msg := range v.Feedback {
 			if text := strings.Split(msg, " "); len(text) >= 5 {
-				vehRating := 5.0
-				vehResult.feedbackTotal ++
+				vehRating = 5.0
+				vehResult.feedbackTotal++
 				for _, word := range text {
-					switch s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r") {
-						case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
-								vehRating += extraPositive
-						case "help", "helpful", "thanks", "thank you", "happy":
-								vehRating += positive
-						case "not helpful", "sad", "angry", "improve", "annoy":
-								vehRating += negative
-						case "pathetic", "bad", "worse", "unfortunately", "agitated", "frustrated":
-								vehRating += extraNegative
+					switch s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r"); s {
+					case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
+						vehRating += extraPositive
+					case "help", "helpful", "thanks", "thank you", "happy":
+						vehRating += positive
+					case "not helpful", "sad", "angry", "improve", "annoy":
+						vehRating += negative
+					case "pathetic", "bad", "worse", "unfortunately", "agitated", "frustrated":
+						vehRating += extraNegative
 					}
 				}
 				switch {
-					case vehRating > 8.0:
-						vehResult.feedbackPositive++
-					case vehRating >= 4.0 && vehRating <= 8.0:
-						vehResult.feedbackNeutral++
-					case vehRating < 4.0:
-						vehResult.feedbackNegative++
+				case vehRating > 8.0:
+					vehResult.feedbackPositive++
+				case vehRating >= 4.0 && vehRating <= 8.0:
+					vehResult.feedbackNeutral++
+				case vehRating < 4.0:
+					vehResult.feedbackNegative++
 				}
 			}
 		}
-	vehResult := vehicleResult[v.Name]
+		vehicleResult[v.Name] = vehResult
 	}
 }
